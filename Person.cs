55c2ecd446033d53c03d5d9638b7cf1c85b7a746
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-05 08:50:29 -0400

    Initial Commit

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance10
+{
+    class Person
+    {
+        private string eyeColor;
+        private int age;
+
+        public string EyeColor {
+            get { return this.eyeColor; }
+            set { this.eyeColor = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
+
+        public Person()
+        {
+
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("This person's age is " + age + " " + " and their eye color is " + eyeColor);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person brad = new Person();
+
+
+            Console.WriteLine("How old are you?");
+            brad.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What color are your eyes?");
+            brad.EyeColor  = Console.ReadLine();
+
+            brad.PrintDetails();
+
+            
+
+       
+        }
+    }
+}
